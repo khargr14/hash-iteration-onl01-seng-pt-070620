36048cf9feec722dbd9350859af88a2c38be2285
Keesha Hargrove <kharg14@gmail.com>
2020-07-16 02:25:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,10 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
+  # add your code snippet here!
+  birthday_kids.each do |kids_name,age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
 
 
