cdd4f9673d98fe4beeecf759e8e71ae59b373776
Keesha Hargrove <kharg14@gmail.com>
2020-07-16 03:20:24 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,18 +12,10 @@ end
 
 def age_appropriate_birthday (birthday_kids)
   birthday_kids.each do |kids_name, age|
+    
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
 
 
-    passengers = {
-      suite_a: "Amanda Presley", 
-      suite_b: "Seymour Hoffman", 
-      suite_c: "Alfred Tennyson", 
-      suite_d: "Charlie Chaplin", 
-      suite_e: "Crumpet the Elf"
-      }
-
    
-  end
