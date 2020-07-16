9b87277fb6708e7e1087671e20506e643883cb46
Keesha Hargrove <kharg14@gmail.com>
2020-07-16 02:58:50 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,4 +10,8 @@
   end
 end
 
-def age_appropriate_birthday 
+def age_appropriate_birthday (birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
